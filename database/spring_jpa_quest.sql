c3b18f352e725d8cf6cd29b8277364a1bcdd50c3
mcdmat <[matheyd@gmail.com](mailto:matheyd@gmail.com)>
2023-03-14 13:28:21 +0100

    solution

--- database/spring_jpa_quest.sql
+++ database/spring_jpa_quest.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `spring_jpa_quest` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `spring_jpa_quest`;
-DROP TABLE IF EXISTS `school`;
-DROP TABLE IF EXISTS `wizard`;
+CREATE DATABASE  IF NOT EXISTS spring_jpa_quest;
+USE spring_jpa_quest;
+DROP TABLE IF EXISTS school;
+DROP TABLE IF EXISTS wizard;
